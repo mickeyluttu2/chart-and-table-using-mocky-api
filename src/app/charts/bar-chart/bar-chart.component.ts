847f03b30710843961c818ff00c13039b11f3b3c
Amal Thomas <56402450+mickeyluttu2@users.noreply.github.com>
2021-09-03 23:07:19 +0530

    Add files via upload

---
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import * as Chart from 'chart.js';
+import { DefaultService } from 'src/app/layouts/default.service';
+
+@Component({
+  selector: 'app-bar-chart',
+  templateUrl: './bar-chart.component.html',
+  styleUrls: ['./bar-chart.component.scss']
+})
+export class BarChartComponent implements OnInit {
+
+  chart = [];
+
+  constructor(private data: DefaultService) { }
+
+  ngOnInit() {
+    this.data.getTableData().subscribe(res => {
+      let labels = res.map((x: { employee: string; }) => x.employee);
+      let values = res.map((x: { salary: number; }) => x.salary);
+      
+      var ctx = document.getElementById('barChart') as HTMLCanvasElement;
+      var barChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: labels,
+          datasets: [
+            { 
+              label: 'Employee Salary',
+              data: values,
+              backgroundColor: ["#0074D9", "#FF4136", "#2ECC40", "#FF851B", "#7FDBFF", "#B10DC9", "#FFDC00", "#001f3f", "#39CCCC", "#01FF70", "#85144b", "#F012BE", "#3D9970", "#111111", "#AAAAAA"],
+              borderColor: ["#0074D9", "#FF4136", "#2ECC40", "#FF851B", "#7FDBFF", "#B10DC9", "#FFDC00", "#001f3f", "#39CCCC", "#01FF70", "#85144b", "#F012BE", "#3D9970", "#111111", "#AAAAAA"],
+              fill: false
+            },
+          ]
+        },
+        options: {
+          legend: {
+            display: true
+          },
+          scales: {
+            xAxes: [{
+              display: true
+            }],
+            yAxes: [{
+              display: true
+            }],
+          }, 
+          animation: {
+            // easing: "easeInSine", 
+            duration: 1000
+          }
+        }
+      });
+      
+    })
+  }
+}
+
